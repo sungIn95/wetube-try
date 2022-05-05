224061fa8a9eef831667865eac4612a9a7fb3f6c
sungIn95 <dlrkehrud@naver.com>
2022-05-05 09:37:33 +0900

    day2: make server

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = () => console.log("somebody is trying to go home.");
+
+app.get("/", handleHome);
+
+const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
