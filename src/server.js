985baa7c033b3a03bade513d4667401d523ef246
sungIn95 <dlrkehrud@naver.com>
2022-05-15 11:52:09 +0900

    error:address already in use

--- src/server.js
+++ src/server.js
@@ -1,23 +1,20 @@
+import "./db"; 
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
-
-console.log(process.cwd());
+const PORT = 4005;
 
 const app = express();
 const logger = morgan("dev");
 
-function hello() {
-  console.log("Hello from Nomad Coders"); 
- }
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter); 
