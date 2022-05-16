4b63ca5e621c72f830869ef4c6e57486387e78e2
sungIn95 <dlrkehrud@naver.com>
2022-05-16 15:39:30 +0900

    123

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,20 +1,12 @@
 import express from "express";
-import { 
-  watch, 
-  getUpload,
-  getEdit, 
-  postEdit, 
-  postUpload
-} from "../controllers/videoController";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
-
-
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
 
- 
--- src/server.js
+++ src/server.js
@@ -4,14 +4,17 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
+
 const app = express();
 const logger = morgan("dev");
 
+function hello() {
+  console.log("Hello from Nomad Coders"); 
+ }
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter); 
